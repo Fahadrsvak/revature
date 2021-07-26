fb75c5f8f36e2066998b1105c36f5c2b0b2c6446
Fahadrsvak <fahadrs1999@gmail.com>
2021-07-26 20:35:18 +0530

    java codes

---
+++ revaturejava practice/myfirstjavaproject/src/mypack/Hello.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("woola!");
+		System.out.print("ihu ");
+		System.out.println("ihu in same  line");
+		System.out.println("ihu in ");
+	}
+
+}
